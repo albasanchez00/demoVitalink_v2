a08a8db245b0f4b2e8b48947943172291529ae9a
albasanchez00 <albasanchezolmedo00@gmail.com>
2025-07-17 19:37:27 +0200

    comentar info sensible

--- src/main/java/com/ceatformacion/demovitalink_v2/services/EmailService.java
+++ src/main/java/com/ceatformacion/demovitalink_v2/services/EmailService.java
@@ -2,6 +2,7 @@ package com.ceatformacion.demovitalink_v2.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
